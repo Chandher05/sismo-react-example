079fc048760f3ac99d5edc47667f15eee2b37cac
Chandher <chandher@designstring.com>
2023-10-13 11:21:15 +0530

    initial loading of sismpo

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { SismoConnectButton, AuthType } from "@sismo-core/sismo-connect-react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const config = {
+        appId: "0x786e741648e610f335dcda57ee15bcf4",
+    };
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <SismoConnectButton
+                    // the client config created
+                    config={config}
+                    // request a proof of account ownership
+                    // (here Vault ownership)
+                    auth={{ authType: AuthType.VAULT }}
+                    // request a proof of group membership
+                    // (here the group with id 0x42c768bb8ae79e4c5c05d3b51a4ec74a)
+                    claim={{ groupId: "0x0f800ff28a426924cbe66b67b9f837e2" }}
+                    // request a message signature
+                    signature={{ message: "Your message" }}
+                    //  a response containing his proofs
+                    onResponse={async (response) => {
+                        console.log({ response });
+                        //Send the response to your server to verify it
+                        //thanks to the @sismo-core/sismo-connect-server package
+                    }}
+                    onResponseBytes={async (bytes) => {
+                        console.log({ bytes });
+                        //Send the response to your contract to verify it
+                        //thanks to the @sismo-core/sismo-connect-solidity package
+                    }}
+                />
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
